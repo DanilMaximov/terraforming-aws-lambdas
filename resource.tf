e45b7f99ccd5833debeb3c20ab5bbc66a99352c4
Nerbyk <danil.maximov2000@gmail.com>
2021-10-20 12:56:22 +0200

    initial commit

---
+++ resource.tf
@@ -0,0 +1,82 @@
+resource "aws_lambda_function" "myLambda" {
+  filename      = "${path.module}/functions.zip"
+  function_name = "hello"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "hello.handler"
+  runtime       = "nodejs12.x"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "role_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_api_gateway_rest_api" "apiLambda" {
+  name = "myAPI"
+}
+
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+  parent_id   = aws_api_gateway_rest_api.apiLambda.root_resource_id
+  path_part   = "index"
+}
+
+resource "aws_api_gateway_method" "proxyMethod" {
+  rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+  resource_id = aws_api_gateway_method.proxyMethod.resource_id
+  http_method = aws_api_gateway_method.proxyMethod.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.myLambda.invoke_arn
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+  resource_id   = aws_api_gateway_rest_api.apiLambda.root_resource_id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_deployment" "apideploy" {
+  depends_on = [
+    aws_api_gateway_integration.lambda
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+  stage_name  = "index"
+}
+
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.myLambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.apiLambda.execution_arn}/*/*"
+}
